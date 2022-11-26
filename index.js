1d493ede6b6948ee2a93f9b23240cf64c0f778ee
Beden <+>
2022-11-26 17:08:27 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solutions in this file
+/*for (let age = 30; age < 40; age++) {
+  console.log(`I'm ${age} years old. Happy birthday to me!`);
+  debugger;
+}*/
+
+/*const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+*/
+
+function writeCards(stringNames, eventName) {
+  let newArray = [];
+
+  for (let i = 0; i < stringNames.length; i++) {
+    let message = `Thank you, ${stringNames[i]}, for the wonderful ${eventName} gift!`;
+    newArray.push(message);
+  }
+  return newArray;
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+function countDown(num) {
+  let i = 10;
+  while (i >= 0) {
+    console.log(i--);
+  }
+}
+countDown(10);
